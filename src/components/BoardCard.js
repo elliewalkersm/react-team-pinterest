e719bedb7e888556c703c91501650aeab05e6922
elliewalkersm <elliewalkersm@gmail.com>
2021-05-10 21:56:21 -0500

    creating board card

---
+++ src/components/BoardCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, Button
+} from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const BoardCard = ({
+  setBoard,
+  uid,
+  ...boardInfo
+}) => {
+  const history = useHistory();
+
+  const viewPins = () => {
+    history.push(`/boards/${boardInfo.id}`);
+  };
+
+  return (
+  <>
+    <Card>
+      <CardBody>
+        <CardTitle tag="h5">{boardInfo.title}</CardTitle>
+        <CardImg top width="100%" src={boardInfo.imageUrl} alt="Card image cap" />
+        <CardText>{boardInfo.description}</CardText>
+        <Button color="danger" onClick={viewPins}>Link</Button>
+      </CardBody>
+    </Card>
+  </>
+  );
+};
+
+BoardCard.propTypes = {
+  boardInfo: PropTypes.object.isRequired,
+  uid: PropTypes.any,
+  setBoard: PropTypes.func.isRequired
+};
+
+export default BoardCard;
--- src/views/Boards.js
+++ src/views/Boards.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import BoardCard from '../components/BoardCard';
+import { getBoards } from '../helpers/data/boardsData';
 
-function Boards() {
+function Boards({ user }) {
+  const [boards, setBoards] = useState({});
+
+  useEffect(() => {
+    getBoards(user?.uid).then((response) => setBoards(response));
+  }, []);
+
+  console.warn(boards);
+
   return (
     <>
-      <h1>View Your Boards Here</h1>
+      <div className="card-container">
+        {boards?.map((boardInfo) => (
+          <BoardCard
+            key={boardInfo.id}
+            {...boardInfo}
+            setBoards={setBoards}
+            user={user}
+          />
+        ))};
+      </div>
     </>
   );
 }
+
+Boards.propTypes = {
+  user: PropTypes.any,
+};
 
 export default Boards;
