9e076248da86372a843b5e6b2805029e5d7d868d
Dani Crosby <dani.crosby1111@gmail.com>
2021-05-17 09:56:49 -0500

    added above card icons, tweaked nav ui

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,12 +22,12 @@ const NavBar = ({ user }) => {
   return (
     <div className="navbar-container">
       <Navbar color="light" light expand="xl" sticky="top">
-        <NavbarBrand href="/"><img src={pinterestBadge} alt="Logo" className="logo" /></NavbarBrand>
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item active"><a className="nav-link" href="https://www.pinterest.com/">Home <span className="sr-only">(current)</span></a></li>
-          <li className="nav-item active"><a className="nav-link" href="#">Favorite <span className="sr-only"></span></a></li>
-        </ul>
-        <Input className="search-bar-input-container ml-2 mr-2" type="text" size="sm" placeholder="search"/>
+        <Nav className="mr-auto" navbar>
+          <NavbarBrand href="/"><img src={pinterestBadge} alt="Logo" className="logo" /></NavbarBrand>
+          <NavItem><Link className="nav-link" href="https://www.pinterest.com/">Home</Link></NavItem>
+          <NavItem><Link className="nav-link" href="#">Favorites</Link></NavItem>
+        </Nav>
+        <Input fluid className="search-bar-input-container ml-2 mr-2" type="text" size="sm" placeholder="search" />
         <NavbarToggler onClick={toggle} /><Collapse isOpen={isOpen} navbar>
 
           <Nav className="mr-auto" navbar>
@@ -37,10 +37,10 @@ const NavBar = ({ user }) => {
           </Nav>
 
           {user !== null && <div className='auth-btn-container'>
-              {user ? <Button className="signOut-btn ml-3" color='danger' size="sm" onClick={signOutUser}>Sign Out</Button>
-                : <Button className="sign-in-btn ml-3" color='danger' size="sm" onClick={signInUser}>Sign In</Button>
-              }
-            </div>
+            {user ? <Button className="signOut-btn ml-3" color='danger' size="sm" onClick={signOutUser}>Sign Out</Button>
+              : <Button className="sign-in-btn ml-3" color='danger' size="sm" onClick={signInUser}>Sign In</Button>
+            }
+          </div>
           }
         </Collapse>
       </Navbar>
--- src/views/Boards.js
+++ src/views/Boards.js
@@ -26,3 +26,14 @@ Boards.propTypes = {
 };
 
 export default Boards;
+
+// Icons above cards
+
+// <div className="boards-view">
+//   <Container className="themed-container icons-header mt-5" fluid={true}>
+//     <Row>
+//       <Col xs="6" className="icons-left d-flex justify-content-start"><i className="fas fa-pencil-alt edit-icon body-icons ml-5"></i><i className="fas fa-upload upload-icon body-icons ml-4"></i></Col>
+//       <Col xs="6" className="icons-right d-flex justify-content-end"><i className="fas fa-align-center filter-icon body-icons"></i><i className="fas fa-plus add-icon body-icons ml-4 mr-5"></i></Col>
+//     </Row>
+//   </Container>
+// </div>
