e526d0f8166aba60ca46c1897738eebda489fc6f
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-05-17 18:10:56 -0500

    commit

--- src/views/Boards.js
+++ src/views/Boards.js
@@ -5,7 +5,7 @@ import BoardCard from '../components/BoardCard';
 function Boards({ user, setBoards, boards }) {
   return (
     <div className="board-card-container">
-      <div className="board-card">
+      <div className="board-card d-flex flex-row w-100 justify-content-around">
         {boards.map((boardInfo) => (
           <BoardCard
             key={boardInfo.id}
