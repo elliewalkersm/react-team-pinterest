84399010b3fae6304d2d15d4807f53d89d202465
Dani Crosby <dani.crosby1111@gmail.com>
2021-05-18 21:41:31 -0500

    fixed form width

--- src/components/PinCard.js
+++ src/components/PinCard.js
@@ -46,14 +46,15 @@ const PinCard = ({
         <CardImg className="card-image" top img-fluid="true" width="100%" src={object.imageUrl} />
         <CardTitle className="card-title mt-4 mb-3" tag="h4">{object.title}</CardTitle>
         <CardText className="card-text mb-4" >{object.description}</CardText>
-        <Button color="light" size="sm"><a href={object.articleLink}>Article</a></Button>
         <hr></hr>
+        <Button color="light" size="sm"><a href={object.articleLink}>Article</a></Button>
         {isPublic ? ''
           : <>
             <Button className="pin-delete-btn" color="light" size="sm" onClick={() => handleClick('delete')}><i className="fas fa-trash mr-2 ml-2"></i></Button>
             <Button board-btn color="light" size="sm" onClick={() => handleClick('edit')}>{editing ? 'Close Form' : 'Edit Pin'}</Button>
           </>
         }
+
         {
           editing && <AddPinForm
             formTitle='Edit Pin'
